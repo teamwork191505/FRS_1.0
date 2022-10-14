65011fd1c91b627f8c530d6a82b365a6a859d337
dhiry <dhiryapatel@gmail.com>
2022-10-15 02:03:08 +0530

    Thursday

--- recognization code/main_video.py
+++ recognization code/main_video.py
@@ -4,10 +4,7 @@ from simple_facerec import SimpleFacerec
 # Encode faces from a folder
 sfr = SimpleFacerec()
 sfr.load_encoding_images("images/")
-
-# Load Camera
 cap = cv2.VideoCapture(0)
-
 
 while True:
     ret, frame = cap.read()
